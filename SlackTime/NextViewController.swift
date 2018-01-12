44354384782c0498df74f78824dab8c05fb69efb
KahoMatsumoto <k.95038chuckle@gmail.com>
2018-01-12 17:39:29 +0900

    きほんのき

---
+++ SlackTime/NextViewController.swift
@@ -0,0 +1,36 @@
+//
+//  NextViewController.swift
+//  SlackTime
+//
+//  Created by Kaho Matsumoto on 2018/01/12.
+//  Copyright © 2018年 Kaho Matsumoto. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
+
