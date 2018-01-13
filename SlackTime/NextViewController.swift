27ddc713c1af2d7894cefece7b2e3cb19fbf1749
KahoMatsumoto <k.95038chuckle@gmail.com>
2018-01-13 15:11:02 +0900

    AI追加した

--- SlackTime/NextViewController.swift
+++ SlackTime/NextViewController.swift
@@ -7,10 +7,37 @@
 //
 
 import UIKit
+import DOCOMOSDKCommon
+import DOCOMOSDKDialogue
 import AVFoundation
+import Speech
+
 
 class NextViewController: UIViewController, AVSpeechSynthesizerDelegate{
     @IBOutlet weak var dateTextField: UITextField!
+    
+    @IBOutlet weak var AItalk: UILabel!
+    
+    @IBOutlet weak var Usertalk: UILabel!
+    @IBOutlet weak var Userbtn: UIButton!
+    
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "ja-JP"))!
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    private let audioEngine = AVAudioEngine()
+    
+    //雑談APIの設定
+    //質問文問い合わせリクエスト
+    var param: DialogueRequestParam!
+    //雑談対話問い合わせ処理
+    var dialogue: Dialogue!
+    //回答データ
+    var resultData: DialogueResultData!
+    //エラー情報
+    var sdkError: SdkError!
+    //var talker: AVSpeechSynthesizer!
+    //音声認識の結果、認識した文字列
+    var voice = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -83,5 +110,165 @@ class NextViewController: UIViewController, AVSpeechSynthesizerDelegate{
      }
      */
     
+}
+
+extension NextViewController: SFSpeechRecognizerDelegate {
+    
+    
+    // 音声認識の可否が変更したときに呼ばれるdelegate
+    func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        if available {
+            Userbtn.isEnabled = true
+            Userbtn.setTitle("音声認識スタート", for: [])
+        } else {
+            Userbtn.isEnabled = false
+            Userbtn.setTitle("音声認識ストップ", for: .disabled)
+        }
+    }
+    public override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        requestRecognizerAuthorization()
+    }
+    
+    private func requestRecognizerAuthorization() {
+        // 認証処理
+        SFSpeechRecognizer.requestAuthorization { authStatus in
+            // メインスレッドで処理したい内容のため、OperationQueue.main.addOperationを使う
+            OperationQueue.main.addOperation { [weak self] in
+                guard let `self` = self else { return }
+                
+                switch authStatus {
+                case .authorized:
+                    self.Userbtn.isEnabled = true
+                    
+                case .denied:
+                    self.Userbtn.isEnabled = false
+                    self.Userbtn.setTitle("音声認識へのアクセスが拒否されています。", for: .disabled)
+                    
+                case .restricted:
+                    self.Userbtn.isEnabled = false
+                    self.Userbtn.setTitle("この端末で音声認識はできません。", for: .disabled)
+                    
+                case .notDetermined:
+                    self.Userbtn.isEnabled = false
+                    self.Userbtn.setTitle("音声認識はまだ許可されていません。", for: .disabled)
+                }
+            }
+        }
+    }
+    
+    private func startRecording() throws {
+        
+        refreshTask()
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        // 録音用のカテゴリをセット
+        try audioSession.setCategory(AVAudioSessionCategoryRecord)
+        try audioSession.setMode(AVAudioSessionModeMeasurement)
+        try audioSession.setActive(true, with: .notifyOthersOnDeactivation)
+        
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        
+        guard let inputNode = audioEngine.inputNode as Optional else { fatalError("Audio engine has no input node") }
+        guard let recognitionRequest = recognitionRequest else { fatalError("Unable to created a SFSpeechAudioBufferRecognitionRequest object") }
+        
+        // 録音が完了する前のリクエストを作るかどうかのフラグ。
+        // trueだと現在-1回目のリクエスト結果が返ってくる模様。falseだとボタンをオフにしたときに音声認識の結果が返ってくる設定。
+        recognitionRequest.shouldReportPartialResults = true
+        
+        recognitionTask = speechRecognizer.recognitionTask(with: recognitionRequest) { [weak self] result, error in
+            
+            
+            guard let `self` = self else { return }
+            
+            var isFinal = false
+            
+            if let result = result {
+                self.voice = result.bestTranscription.formattedString;
+                self.Usertalk.text = self.voice
+                isFinal = result.isFinal
+            }
+            // エラーがある、もしくは最後の認識結果だった場合の処理
+            if error != nil || isFinal {
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+                
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+                
+                self.Userbtn.isEnabled = true
+                self.Userbtn.setTitle("しゃべりかける", for: [])
+                self.AI()
+            }
+        }
+        
+        // マイクから取得した音声バッファをリクエストに渡す
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer: AVAudioPCMBuffer, when: AVAudioTime) in
+            self.recognitionRequest?.append(buffer)
+        }
+        
+        try startAudioEngine()
+    }
+    
+    private func refreshTask() {
+        if let recognitionTask = recognitionTask {
+            recognitionTask.cancel()
+            self.recognitionTask = nil
+        }
+    }
+    private func startAudioEngine() throws {
+        // startの前にリソースを確保しておく。
+        audioEngine.prepare()
+        
+        try audioEngine.start()
+        
+        Usertalk.text = "どうぞ喋ってください。"
+    }
+    @IBAction func tappedStartButton(_ sender: AnyObject) {
+        if audioEngine.isRunning {
+            audioEngine.stop()
+            recognitionRequest?.endAudio()
+            Userbtn.isEnabled = false
+            Userbtn.setTitle("停止中", for: .disabled)
+        } else {
+            try! startRecording()
+            Userbtn.setTitle("しゃべるのをやめる", for: [])
+        }
+    }
+    
+    func AI(){
+        //認証情報初期化
+        //docomo Developer supportから取得したAPIキーを設定
+        AuthApiKey.initializeAuth("79705866567a64506554337741335543613169765243677258464b6b544454687039492e69334f5072392e")
+        
+        param = DialogueRequestParam()
+        dialogue = Dialogue()
+        sdkError = SdkError()
+        //talker = AVSpeechSynthesizer()
+        //発話を設定
+        param.utt = voice
+        //APIのキャラクタ設定(デフォルト:ゼロ,20:桜子,30:ハヤテ)
+        //param.character = 0
+        
+        /*
+         雑談対話問い合わせ処理にデータを渡す.
+         APIからは音声合成用情報が「resultData.yomi」に返ってくるので
+         AVSpeechSynthesizerで読み上げる.
+         コンテキストID(resultData.context)を使うことで継続した会話ができる.
+         */
+        dialogue.request(param, onComplete: { (resultData) -> Void in
+            self.param.context = "\(resultData!.context)"
+            self.AItalk.text = resultData!.yomi
+            // let utterance = AVSpeechUtterance(string: "\(resultData!.yomi)")
+            //utterance.voice = AVSpeechSynthesisVoice(language: "jp-JP")
+            //utterance.rate = 0.2
+            //utterance.pitchMultiplier = 1.4
+            //self.talker.speak(utterance)
+        }) { (sdkError) -> Void in
+            print("\(sdkError!)")
+        }
+    }
 }
 
